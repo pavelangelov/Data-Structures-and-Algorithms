5f5a68245eaf97b0c5ebf1fe138917f0275b96c5
pavelangelov <p_a_v_el@abv.bg>
2016-11-23 00:08:35 +0200

    Task 2.

---
+++ 02. Linear-Data-Structures/02.ReverseNumbers/Startup.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+using Utils.Contracts;
+using Utils.Models;
+
+namespace _02.ReverseNumbers
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            ILogger logger = new ConsoleLogger();
+            IReader reader = new ConsoleReader();
+
+            Stack<int> numbers = GetNumbers(reader, logger);
+
+            PrintReversedNumbers(numbers, logger);
+        }
+
+        private static void PrintReversedNumbers(Stack<int> numbers, ILogger logger)
+        {
+            logger.Write("[ ");
+            while (numbers.Count > 0)
+            {
+                if (numbers.Count == 1)
+                {
+                    logger.Write(numbers.Pop().ToString());
+                    break;
+                }
+                logger.Write($"{numbers.Pop()}, ");
+            }
+
+            logger.Write(" ]");
+            logger.WriteLine();
+        }
+
+        private static Stack<int> GetNumbers(IReader reader, ILogger logger)
+        {
+            var input = reader.ReadLine();
+            var numbers = new Stack<int>();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                int number;
+
+                if (int.TryParse(input, out number))
+                {
+                    numbers.Push(number);
+                }
+                else
+                {
+                    logger.WriteLine($"The passed string '{input}' is not convertible to integer! Try again.");
+                }
+
+                input = reader.ReadLine();
+            }
+
+            return numbers;
+        }
+    }
+}
