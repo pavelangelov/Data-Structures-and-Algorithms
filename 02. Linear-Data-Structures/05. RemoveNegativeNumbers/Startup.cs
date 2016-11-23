b810d80925e848b2e3618579f1362b084bbf0d3f
pavelangelov <p_a_v_el@abv.bg>
2016-11-23 14:52:27 +0200

    Solved Task 5, 6.

---
+++ 02. Linear-Data-Structures/05. RemoveNegativeNumbers/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System.Linq;
+
+using Utils.Models;
+
+namespace _05.RemoveNegativeNumbers
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var reader = new ConsoleReader();
+            var logger = new ConsoleLogger();
+
+            var numbers = TaskHelper.GetNumbers(reader, logger)
+                                    .Where(x => x >= 0)
+                                    .ToList();
+
+            logger.WriteLine(string.Join(", ", numbers));
+        }
+    }
+}
---
+++ 02. Linear-Data-Structures/06. RemoveNumberIfOccureOddNumberOfTimes/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+using Utils.Models;
+
+namespace _06.RemoveNumberIfOccureOddNumberOfTimes
+{
+    public class Startup
+    {
+        public static void Main()
+        {
+            var logger = new ConsoleLogger();
+            var occureKeys = new Dictionary<int, int>();
+            var numbers = new List<int>() { 4, 2, 2, 5, 2, 3, 2, 3, 1, 5, 2 };
+
+            foreach (var number in numbers)
+            {
+                if (occureKeys.ContainsKey(number))
+                {
+                    occureKeys[number]++;
+                }
+                else
+                {
+                    occureKeys.Add(number, 1);
+                }
+            }
+
+            foreach (var number in numbers)
+            {
+                if (occureKeys[number] % 2 == 0)
+                {
+                    logger.Write(number + " ");
+                }
+            }
+
+            logger.WriteLine();
+        }
+    }
+}
