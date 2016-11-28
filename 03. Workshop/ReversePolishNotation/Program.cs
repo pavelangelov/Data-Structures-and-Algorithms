4192a3f50f33beadb3b129b784612901b4a3a8eb
pavelangelov <p_a_v_el@abv.bg>
2016-11-29 00:42:12 +0200

    Reverse-Polish-Notation solved

---
+++ 03. Workshop/ReversePolishNotation/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversePolishNotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine().Split(' ');
+
+            var numbers = new Stack<long>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                int number;
+                if (int.TryParse(input[i], out number))
+                {
+                    numbers.Push(number);
+                }
+                else
+                {
+                    var sign = input[i];
+                    var secondValue = numbers.Pop();
+                    var firstValue = numbers.Pop();
+
+                    var result = ProceedCalculation(sign, firstValue, secondValue);
+
+                    numbers.Push(result);
+                }
+            }
+
+            Console.WriteLine(numbers.Pop());
+        }
+
+        private static long ProceedCalculation(string operation, long firstValue, long secondValue)
+        {
+            long result;
+            switch (operation)
+            {
+                case "+":
+                    result = firstValue + secondValue;
+                    break;
+                case "-":
+                    result = firstValue - secondValue;
+                    break;
+                case "*":
+                    result = firstValue * secondValue;
+                    break;
+                case "/":
+                    result = firstValue / secondValue;
+                    break;
+                case "&":
+                    result = firstValue & secondValue;
+                    break;
+                case "|":
+                    result = firstValue | secondValue;
+                    break;
+                case "^":
+                    result = firstValue ^ secondValue;
+                    break;
+                default:
+                    throw new ArgumentException("operator");
+            }
+
+            return result;
+        }
+    }
+}
