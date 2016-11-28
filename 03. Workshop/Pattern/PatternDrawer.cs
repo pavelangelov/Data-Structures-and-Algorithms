404c282b006eef1ca4f714ef0d22e2c03ef8fd86
pavelangelov <p_a_v_el@abv.bg>
2016-11-29 00:04:26 +0200

    Pattern solved

---
+++ 03. Workshop/Pattern/PatternDrawer.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pattern
+{
+    class PatternDrawer
+    {
+        private string figure = "urd";
+
+        public string GetFigure(int interations)
+        {
+            GetNextFigure(interations - 1);
+
+            return this.figure;
+        }
+
+        public void GetNextFigure(int interations)
+        {
+            if (interations == 0)
+            {
+                return;
+            }
+
+            var next = new StringBuilder();
+
+            // append figure turned right
+            for (int i = figure.Length - 1; i >= 0 ; i--)
+            {
+                var letter = figure[i];
+                next.Append(ReverseRight(letter.ToString()));
+            }
+
+            // turn up
+            next.Append("u");
+
+            // append new figure
+            next.Append(this.figure);
+
+            // turn right
+            next.Append("r");
+
+            // append new figure
+            next.Append(this.figure);
+
+            // turn down
+            next.Append("d");
+
+            // append figure turned left
+            for (int i = figure.Length - 1; i >= 0; i--)
+            {
+                var letter = figure[i];
+                next.Append(ReverseLeft(letter.ToString()));
+            }
+
+            figure = next.ToString();
+
+            GetNextFigure(interations - 1);
+        }
+
+        private string ReverseRight(string letter)
+        {
+            switch (letter)
+            {
+                case "u":
+                    return "l";
+                case "r":
+                    return "u";
+                case "d":
+                    return "r";
+                case "l":
+                    return "d";
+                default:
+                    throw new ArgumentException("letter");
+            }
+        }
+
+        private string ReverseLeft(string letter)
+        {
+            switch (letter)
+            {
+                case "u":
+                    return "r";
+                case "r":
+                    return "d";
+                case "d":
+                    return "l";
+                case "l":
+                    return "u";
+                default:
+                    throw new ArgumentException("letter");
+            }
+        }
+    }
+}
---
+++ 03. Workshop/Pattern/Startup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Pattern
+{
+    class Startup
+    {
+
+        static void Main(string[] args)
+        {
+            var input = int.Parse(Console.ReadLine());
+            var pattern = new PatternDrawer().GetFigure(input);
+
+            Console.WriteLine(pattern);
+        }
+    }
+}
