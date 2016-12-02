d383869ef50cf3717555770693f03d6f9edfdcf9
pavelangelov <p_a_v_el@abv.bg>
2016-12-02 15:27:51 +0200

    Added solution for second task.

---
+++ 04. Combinatorics/02.Binomial theorem/Binominal.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+
+using Framework.Contracts;
+
+namespace _02.Binomial_theorem
+{
+    public class Binominal
+    {
+        private List<string> result;
+        private int startPow;
+        private char first;
+        private char second;
+
+        public void Start(IReader reader, ILogger logger)
+        {
+            var sequence = reader.ReadLine();
+            this.first = sequence[1];
+            this.second = sequence[3];
+            this.startPow = int.Parse(reader.ReadLine());
+
+            this.result = new List<string>();
+            this.result.Add($"({this.first}^{this.startPow})");
+
+            ExtractPow(this.startPow, 0, this.startPow, 1);
+
+            logger.WriteLine(string.Join("+", this.result));
+        }
+
+        private void ExtractPow(int firstPow, int secondPow, long lastProduct, long lastDevider)
+        {
+
+            if (firstPow <= 1)
+            {
+                this.result.Add($"({this.second}^{this.startPow})");
+                return;
+            }
+            else
+            {
+                long update;
+                if (this.startPow == firstPow)
+                {
+                    update = 1;
+                }
+                else
+                {
+                    update = this.startPow - secondPow;
+                }
+
+                var product = (lastProduct * update) / lastDevider++;
+                this.result.Add($"{product}({this.first}^{firstPow - 1})({this.second}^{secondPow + 1})");
+                ExtractPow(firstPow - 1, secondPow + 1, product, lastDevider);
+            }
+        }
+    }
+}
--- 04. Combinatorics/ConsoleClient/Startup.cs
+++ 04. Combinatorics/ConsoleClient/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using _01.SumOfSubsequence;
+using _02.Binomial_theorem;
 using ConsoleClient.Models;
 using Framework.Models;
 
@@ -10,17 +11,41 @@ namespace ConsoleClient
         {
             var logger = new ConsoleLogger();
             var reader = new ConsoleReader();
+
+            // ============================================================================
+            // Task 1. Sum of subsequences.
+            var pr = new SubSequence(logger);
+            pr.Calculate(reader);
+
             /* 
+             * Test 1.
                 4 2	
                 numbers => 1 2 3 4
                 result = 30
 
+                Test 2.
                 5 3
                 numbers => 1 –5 7 10 –3
                 result 40
             */
-            var pr = new SubSequence(logger);
-            pr.Calculate(reader);
+
+            // ============================================================================
+            // Task 2. (firstNumber + secondNumber) ^ n
+            var binominal = new Binominal();
+            binominal.Start(reader, logger);
+
+            /*
+             * Test 1.
+                (a+z)
+                1	
+                result => (a^1)+(z^1)
+                
+                Test 2.
+                (c+y)
+                3	
+                result => (c^3)+3(c^2)(y^1)+3(c^1)(y^2)+(y^3)
+
+             */
         }
     }
 }
