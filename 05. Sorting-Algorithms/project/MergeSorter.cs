d9ca4464df207681e4851c68ac81a3158576eff6
pavelangelov <p_a_v_el@abv.bg>
2016-12-07 17:32:19 +0200

    Implement Merge sort

--- 05. Sorting-Algorithms/project/MergeSorter.cs
+++ 05. Sorting-Algorithms/project/MergeSorter.cs
@@ -10,7 +10,60 @@
     {
         public void Sort(IList<T> collection)
         {
-            throw new NotImplementedException();
+            MergeSort(collection);
+        }
+
+        private void MergeSort(IList<T> collection)
+        {
+            if (collection.Count <= 1)
+            {
+                return;
+            }
+
+            int middleIndex = collection.Count / 2;
+            IList<T> left = collection.Take(middleIndex).ToList();
+            IList<T> right = collection.Skip(middleIndex).ToList();
+
+            MergeSort(left);
+            MergeSort(right);
+
+            Merge(collection, left, right);
+        }
+
+        private void Merge(IList<T> collection, IList<T> left, IList<T> right)
+        {
+            var leftIndex = 0;
+            var rightIndex = 0;
+            int collectionIndex = 0;
+            for (; collectionIndex < collection.Count; collectionIndex++)
+            {
+
+                if (leftIndex >= left.Count || rightIndex > right.Count)
+                {
+                    break;
+                }
+
+                if (left[leftIndex] .CompareTo(right[rightIndex]) <= 0)
+                {
+                    collection[collectionIndex] = left[leftIndex];
+                    leftIndex++;
+                }
+                else
+                {
+                    collection[collectionIndex] = right[rightIndex];
+                    rightIndex++;
+                }
+            }
+
+            for (int i = leftIndex; i < left.Count; i++, collectionIndex++)
+            {
+                collection[collectionIndex] = left[leftIndex];
+            }
+
+            for (int i = rightIndex; i < right.Count; i++, collectionIndex++)
+            {
+                collection[collectionIndex] = right[rightIndex];
+            }
         }
     }
 }
