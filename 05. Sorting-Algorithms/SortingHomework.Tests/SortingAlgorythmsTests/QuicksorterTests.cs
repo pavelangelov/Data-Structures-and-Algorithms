bff69f1093076be0b5bf7789b21ce0ecf8f51a6d
pavelangelov <p_a_v_el@abv.bg>
2016-12-07 20:16:08 +0200

    Added Unit Tests for sorting algorythms and fixed bug in MergeSorter

---
+++ 05. Sorting-Algorithms/SortingHomework.Tests/SortingAlgorythmsTests/QuicksorterTests.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+
+using NUnit.Framework;
+
+namespace SortingHomework.Tests.SortingAlgorythmsTests
+{
+    [TestFixture]
+    public class QuicksorterTests
+    {
+        [Test]
+        public void SortShould_SortThePassedCollectionOfIntegers()
+        {
+            // Arange
+            var sorter = new Quicksorter<int>();
+            var collection = new List<int>() { 2, 1, 4, 5, 6, 3 };
+            var result = "1, 2, 3, 4, 5, 6";
+
+            // Act
+            sorter.Sort(collection);
+
+            // Assert
+            Assert.AreEqual(result, string.Join(", ", collection));
+        }
+
+        [Test]
+        public void SortShould_SortThePassedCollectionOfStrings()
+        {
+            // Arange
+            var sorter = new Quicksorter<string>();
+            var collection = new List<string>() { "2", "1", "4", "5", "6", "3" };
+            var result = "1, 2, 3, 4, 5, 6";
+
+            // Act
+            sorter.Sort(collection);
+
+            // Assert
+            Assert.AreEqual(result, string.Join(", ", collection));
+        }
+
+        [Test]
+        public void SortShould_NotThrowIfTheCollectionIsEmpty_AndReturnEmptyCollection()
+        {
+            // Arange
+            var sorter = new Quicksorter<string>();
+            var collection = new List<string>(0);
+
+            // Act
+            sorter.Sort(collection);
+
+            // Assert
+            Assert.AreEqual("", string.Join(", ", collection));
+        }
+    }
+}
