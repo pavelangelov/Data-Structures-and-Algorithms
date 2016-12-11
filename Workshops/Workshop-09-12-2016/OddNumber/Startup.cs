742020671b2cb4affcf4e003329b2fa92e23517a
pavelangelov <p_a_v_el@abv.bg>
2016-12-11 17:26:17 +0200

    Initial commit

---
+++ Workshops/Workshop-09-12-2016/OddNumber/Startup.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddNumber
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var numbers = new HashSet<long>();
+            var n = long.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                var number = long.Parse(Console.ReadLine());
+                if (numbers.Contains(number))
+                {
+                    numbers.Remove(number);
+                }
+                else
+                {
+                    numbers.Add(number);
+                }
+            }
+
+            Console.WriteLine(numbers.First());
+        }
+    }
+}
