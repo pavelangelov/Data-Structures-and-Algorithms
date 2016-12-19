4f5165e503c2dad8ed7b8d3126f3917b56adca36
pavelangelov <p_a_v_el@abv.bg>
2016-12-19 21:00:03 +0200

    PriorityQueue task

---
+++ 14. Advanced-Data-Structures/CLI/Startup.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+using Framework;
+
+namespace CLI
+{
+    class Startup
+    {
+        static void Main()
+        {
+            var queue = new PriorityQueue<int>(2);
+
+            FillQueue(queue, 10);
+
+            EmptyTheQueue(queue);
+
+            // Check if there is extra values :)
+            Console.WriteLine($"Remove: {queue.Pull()}");
+            Console.WriteLine($"Current Top: {queue.Peek()}");
+
+            // Check if it`s strill working
+            FillQueue(queue, 3);
+            EmptyTheQueue(queue);
+        }
+
+        private static void FillQueue(PriorityQueue<int> queue, int numberOfElements)
+        {
+            for (int i = 1; i <= numberOfElements; i++)
+            {
+                Console.WriteLine($"Insert {i} in the Queue.");
+                queue.Insert(i);
+                Console.WriteLine($"Current Top: {queue.Peek()}");
+            }
+        }
+
+        private static void EmptyTheQueue(PriorityQueue<int> queue)
+        {
+            var length = queue.Size;
+            for (int i = 0; i < length; i++)
+            {
+                Console.WriteLine($"Remove: {queue.Pull()}");
+                Console.WriteLine($"Current Top: {queue.Peek()}");
+            }
+        }
+    }
+}
---
+++ 14. Advanced-Data-Structures/Framework/PriorityQueue.cs
@@ -0,0 +1,100 @@
+﻿using System;
+
+namespace Framework
+{
+    public class PriorityQueue<T> where T: IComparable<T>
+    {
+        private T[] queue;
+        private int size = 0;
+
+        public PriorityQueue(int length)
+        {
+            this.queue = new T[length];
+        }
+
+        public int Size { get { return this.size; } }
+
+        public void Insert(T item)
+        {
+            if (this.size == this.queue.Length)
+            {
+                this.Resize();
+            }
+
+            this.queue[this.size] = item;
+            ResortQueue(this.size);
+            this.size++;
+        }
+
+        public T Peek()
+        {
+            return this.queue[0];
+        }
+
+        public T Pull()
+        {
+            T item = this.queue[0];
+            if (this.size > 1)
+            {
+                this.size--;
+                this.queue[0] = this.queue[this.size];
+
+                RebalanceQueue(0);
+            }
+            else
+            {
+                this.queue[0] = default(T);
+            }
+
+            return item;
+        }
+
+        private void RebalanceQueue(int parentIndex)
+        {
+            var parentItem = this.queue[parentIndex];
+            var leftChildIndex = parentIndex * 2;
+            var rightChildIndex = parentIndex * 2 + 1;
+            var largestChildIndex = parentIndex;
+
+            if (leftChildIndex < this.size && this.queue[leftChildIndex].CompareTo(this.queue[largestChildIndex]) > 0)
+            {
+                largestChildIndex = leftChildIndex;
+            }
+
+            if (rightChildIndex < this.size && this.queue[rightChildIndex].CompareTo(this.queue[largestChildIndex]) > 0)
+            {
+                largestChildIndex = rightChildIndex;
+            }
+
+            if (largestChildIndex != parentIndex)
+            {
+                var temp = this.queue[parentIndex];
+                this.queue[parentIndex] = this.queue[largestChildIndex];
+                this.queue[largestChildIndex] = temp;
+                this.RebalanceQueue(largestChildIndex);
+            }
+        }
+
+        private void ResortQueue(int childIndex)
+        {
+            if (childIndex > 0)
+            {
+                var parentIndex = childIndex / 2;
+                if (this.queue[parentIndex].CompareTo(this.queue[childIndex]) < 0)
+                {
+                    var temp = this.queue[parentIndex];
+                    this.queue[parentIndex] = this.queue[childIndex];
+                    this.queue[childIndex] = temp;
+                    this.ResortQueue(parentIndex);
+                }
+            }
+        }
+
+        private void Resize()
+        {
+            var resizedQueue = new T[this.queue.Length * 2];
+            Array.Copy(this.queue, 0, resizedQueue, 0, this.queue.Length);
+            this.queue = resizedQueue;
+        }
+    }
+}
