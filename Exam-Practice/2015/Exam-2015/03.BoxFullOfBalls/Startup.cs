65aeff3dc0bd0867f85c39bdc664206d39f48531
pavelangelov <p_a_v_el@abv.bg>
2017-01-08 02:36:25 +0200

    Added solution

--- Exam-Practice/2015/Exam-2015/03.BoxFullOfBalls/Startup.cs
+++ Exam-Practice/2015/Exam-2015/03.BoxFullOfBalls/Startup.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace _03.BoxFullOfBalls
 {
@@ -18,37 +15,36 @@ namespace _03.BoxFullOfBalls
                                 .Split(' ')
                                 .Select(int.Parse)
                                 .ToArray();
+            
+            var end = balls[1];
+            var isWin = new bool[end + 1];
+
+            for (int i = 1; i <= end; i++)
+            {
+                foreach (var move in possibleTurns)
+                {
+                    if (move > i)
+                    {
+                        continue;
+                    }
+
+                    if (!isWin[i - move])
+                    {
+                        isWin[i] = true;
+                    }
+                }
+            }
+
             var wins = 0;
             for (int i = balls[0]; i <= balls[1]; i++)
             {
-                if (Game(i, possibleTurns, true))
+                if (isWin[i])
                 {
                     wins++;
                 }
             }
 
             Console.WriteLine(wins);
-        }
-
-        static bool Game(int numberOfBalls, int[] possibleTurns, bool isFirstPlayer)
-        {
-            if (numberOfBalls <= 0)
-            {
-                return false;
-            }
-            foreach (var turn in possibleTurns)
-            {
-                if (turn > numberOfBalls)
-                {
-                    continue;
-                }
-                if(!Game(numberOfBalls - turn, possibleTurns, !isFirstPlayer))
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
     }
 }
