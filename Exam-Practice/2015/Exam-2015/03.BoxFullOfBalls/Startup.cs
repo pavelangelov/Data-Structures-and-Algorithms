2731021d5beffac55c2d6e960548beee02d9622d
pavelangelov <p_a_v_el@abv.bg>
2017-01-08 02:22:18 +0200

    Added slow solution

--- Exam-Practice/2015/Exam-2015/03.BoxFullOfBalls/Startup.cs
+++ Exam-Practice/2015/Exam-2015/03.BoxFullOfBalls/Startup.cs
@@ -10,6 +10,45 @@ namespace _03.BoxFullOfBalls
     {
         static void Main(string[] args)
         {
+            var possibleTurns = Console.ReadLine()
+                                        .Split(' ')
+                                        .Select(int.Parse)
+                                        .ToArray();
+            var balls = Console.ReadLine()
+                                .Split(' ')
+                                .Select(int.Parse)
+                                .ToArray();
+            var wins = 0;
+            for (int i = balls[0]; i <= balls[1]; i++)
+            {
+                if (Game(i, possibleTurns, true))
+                {
+                    wins++;
+                }
+            }
+
+            Console.WriteLine(wins);
+        }
+
+        static bool Game(int numberOfBalls, int[] possibleTurns, bool isFirstPlayer)
+        {
+            if (numberOfBalls <= 0)
+            {
+                return false;
+            }
+            foreach (var turn in possibleTurns)
+            {
+                if (turn > numberOfBalls)
+                {
+                    continue;
+                }
+                if(!Game(numberOfBalls - turn, possibleTurns, !isFirstPlayer))
+                {
+                    return true;
+                }
+            }
+
+            return false;
         }
     }
 }
